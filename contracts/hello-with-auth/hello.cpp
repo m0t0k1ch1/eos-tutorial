d8530794180a12b5138e37e684dd471adfa10222
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-01-27 22:11:02 +0900

    add hello-with-auth contract

---
+++ contracts/hello-with-auth/hello.cpp
@@ -0,0 +1,20 @@
+// include the required libraries
+#include <eosiolib/eosio.hpp> // loads the EOSIO C and C++ API
+#include <eosiolib/print.hpp>
+
+// use namespace eosio
+using namespace eosio;
+
+class hello : public contract { // the contract class needs to extend eosio::contract
+public: // public access specifier
+  using contract::contract; // will allow us to write more concise code
+
+  [[eosio::action]] // inform the hi() action to the abi generator
+  void hi(name user) { // name is one of the most common typedefs
+    require_auth(user); // check if the user executing the action matches the provided parameter
+    print("Hello, ", user);
+  }
+};
+
+// handle the dispatching of actions for the hello contract
+EOSIO_DISPATCH(hello, (hi));
