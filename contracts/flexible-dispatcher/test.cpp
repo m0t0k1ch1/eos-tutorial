4da04f423dc66408d7b8a6e281a0bb14462865e7
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-02-13 17:25:15 +0900

    add test contract using the flexible dispatcher

---
+++ contracts/flexible-dispatcher/test.cpp
@@ -0,0 +1,49 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+
+using namespace eosio;
+
+class test : public contract {
+public:
+  using contract::contract;
+
+  [[eosio::action]]
+  void a(name user) {
+    require_auth(user);
+    print("Hi, ", user, " from a");
+  }
+
+  [[eosio::action]]
+  void b(name user) {
+    require_auth(user);
+    print("Hi, ", user, " from b");
+  }
+
+  [[eosio::action]]
+  void c(name user) {
+    require_auth(user);
+    print("Hi, ", user, " from c");
+  }
+
+  [[eosio::action]]
+  void notify(name user, const std::string msg) {
+    print(msg);
+  }
+};
+
+extern "C" {
+  void apply(uint64_t receiver, uint64_t code, uint64_t action) {
+    if (code == receiver && action == name("a").value) {
+      execute_action(name(receiver), name(code), &test::a);
+    }
+    else if (code == receiver && action == name("b").value) {
+      execute_action(name(receiver), name(code), &test::b);
+    }
+    else if (code == receiver && action == name("c").value) {
+      execute_action(name(receiver), name(code), &test::c);
+    }
+    else if (code == name("addressbook").value && action == name("notify").value) {
+      execute_action(name(receiver), name(code), &test::notify);
+    }
+  }
+};
